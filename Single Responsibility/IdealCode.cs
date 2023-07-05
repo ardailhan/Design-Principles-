58e6b9a8ea62701b23cb186f1d96547f6b38cf02
ardailhan <ardailhan_@outlook.com>
2023-07-05 14:56:05 +0300

    SingleResponsiblityPrinciple

---
+++ Single Responsibility/IdealCode.cs
@@ -0,0 +1,38 @@
+﻿using Single_Responsibility;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Single_Responsibility_IdealCode
+{
+    class Database
+    {
+        public void Connect()
+        {
+            //...
+            Console.WriteLine("Veritabanı bağlantısı sağlanmıştır.");
+        }
+
+        public void Disconnect()
+        {
+            //...
+            Console.WriteLine("Veritabanı bağlantısı kesilmiştir.");
+        }
+        public string State { get; set; }
+    }
+
+    class PersonService
+    {
+        public List<Person> GetPersons()
+        {
+            return new()
+            {
+                new(){Name = "Arda", Surname = "İlhan"},
+                new(){Name = "Falan", Surname = "İlhan"},
+                new(){Name = "Filan", Surname = "İlhan"}
+            };
+        }
+    }
+}
---
+++ Single Responsibility/NotIdealCode.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Single_Responsibility
+{
+    class Database
+    {
+        public void Connect()
+        {
+            //...
+            Console.WriteLine("Veritabanı bağlantısı sağlanmıştır.");
+        }
+        public void Disconnect()
+        {
+            //...
+            Console.WriteLine("Veritabanı bağlantısı kesilmiştir.");
+        }
+        public string State { get; set; }
+        public List<Person> GetPersons()
+        {
+            return new()
+            {
+            new(){ Name = "Arda", Surname = "İlhan" },
+            new(){ Name = "Falan", Surname = "İlhan" },
+            new(){ Name = "Filan", Surname = "İlhan" }
+            };
+
+        }
+    }
+}
