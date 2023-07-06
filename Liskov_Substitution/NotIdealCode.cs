94233944ecb0cc246d41e157539744b79a59d916
ardailhan <ardailhan_@outlook.com>
2023-07-06 13:16:56 +0300

    Liskov_Substitution

---
+++ Liskov_Substitution/NotIdealCode.cs
@@ -0,0 +1,43 @@
+﻿using Liskov_Substitution_IdealCode;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Liskov_Substitution_NotIdealCode
+{
+    abstract class Cloud
+    {
+        public abstract void Translate();
+        public abstract void MachineLearning();
+    }
+
+    class AWS : Cloud
+    {
+        public override void Translate()
+            => Console.WriteLine("AWS Translate");
+        public override void MachineLearning()
+            => Console.WriteLine("AWS Machine Learning");
+    }
+
+    class Azure : Cloud
+    {
+        public override void Translate()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void MachineLearning()
+            => Console.WriteLine("Azure Machine Learning");
+    }
+
+    class Google : Cloud
+    {
+        public override void Translate()
+            => Console.WriteLine("Google Translate");
+
+        public override void MachineLearning()
+            => Console.WriteLine("Google Machine Learning");
+    }
+}
---
+++ Liskov_Substitution/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Liskov_Substitution
+{
+    #region Not Ideal Code
+    //using Liskov_Substitution_Principle_NotIdealCode;
+    //Cloud cloud = new AWS();
+    //cloud.MachineLearning();
+    //cloud.Translate();
+
+    //cloud = new Google();
+    //cloud.MachineLearning();
+    //cloud.Translate();
+
+    //cloud = new Azure();
+    //cloud.MachineLearning();
+    //if (cloud is not Azure)
+    //{
+    //    cloud.Translate();
+    //}
+    #endregion
+
+    #region Ideal Code
+    //using Liskov_Substitution_Principle_IdealCode;
+    //Cloud cloud = new AWS();
+    //cloud.MachineLearning();
+    //(cloud as ITranslatable)?.Translate();
+
+    //cloud = new Google();
+    //cloud.MachineLearning();
+    //(cloud as ITranslatable)?.Translate();
+
+    //cloud = new Azure();
+    //cloud.MachineLearning();
+    //(cloud as ITranslatable)?.Translate();
+    #endregion
+}
