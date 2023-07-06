fc4879b64ebb55764c03a00f42d7ffc3c0be0a9c
ardailhan <ardailhan_@outlook.com>
2023-07-06 20:03:26 +0300

    Interface_Segregation

---
+++ Interface_Segregation/Program.cs
@@ -0,0 +1,11 @@
+﻿using Interface_Segregation_IdealCode;
+//using Interface_Segregation_Principle_NotIdealCode;
+
+#region Not Ideal Code
+//SamsungPrinter printer = new();
+//printer.PrintDuplex();
+#endregion
+#region Ideal Code
+SamsungPrinter printer = new();
+printer.Fax();
+#endregion
